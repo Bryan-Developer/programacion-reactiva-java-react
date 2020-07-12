8db3c91a05ef2487aae6a8a51504ebdf1f9e823d
Bryan Antonio Alvarado Villalobos <bryan.developer.alvarado@gmail.com>
2020-07-12 01:27:41 -0500

    Starting Coding

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-BRANCH master
 BRANCH development
---
+++ spring-boot-reactivo/src/main/java/bryan/developer/curso/programacion/reactiva/springbootreactivo/DemoApplication.java
@@ -0,0 +1,13 @@
+package bryan.developer.curso.programacion.reactiva.springbootreactivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
