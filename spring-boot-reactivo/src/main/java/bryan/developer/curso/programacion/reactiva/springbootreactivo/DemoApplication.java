fe5d1e96ff6dbdf66436518a69d6d62050b4a518
Bryan Antonio Alvarado Villalobos <bryan.developer.alvarado@gmail.com>
2020-07-12 21:00:08 -0500

    Fixing Errors

--- spring-boot-reactivo/src/main/java/bryan/developer/curso/programacion/reactiva/springbootreactivo/DemoApplication.java
+++ spring-boot-reactivo/src/main/java/bryan/developer/curso/programacion/reactiva/springbootreactivo/DemoApplication.java
@@ -1,13 +1,22 @@
 package bryan.developer.curso.programacion.reactiva.springbootreactivo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Andres", "Bryan", "Marco", "Mercedes", "Romina", "Pedro", "Fiorella", "Anita")
+				.doOnNext(System.out::println);
+		nombres.subscribe();
+	}
 }
